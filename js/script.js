c9d60fb3fbee2de4ef1670512a261452d76b91a6
markmumba <markmumba01@gmail.com>
2019-06-10 10:50:52 +0300

    hover effect

--- js/script.js
+++ js/script.js
@@ -2,20 +2,39 @@ $(document).ready(function () {
     $("#design img").show();
     $("#design").click(function () {
         $("#design p").toggle();
-        $("#design img").toggle();
+       
     });
 });
 $(document).ready(function () {
     $("#development").show();
     $("#development").click(function () {
         $("#development p").toggle();
-        $("#development img").toggle();
+
     });
 });
 $(document).ready(function () {
     $("#product").show();
     $("#product").click(function () {
         $("#product p").toggle();
-        $("#product img").toggle();
+     
     });
+});
+
+$(document).ready(function() {
+    $('.text').hide();
+    $('img').animate({
+        opacity:1
+        
+    });
+
+    $('img').hover(function() {
+        $(this).stop().animate({opacity:.4},200);
+        $('.text').fadeIn();
+
+    }, function() {
+        $(this).stop().animate({opacity:1},500)
+        $('.text').fadeOut();
+    });
+
+
 });
