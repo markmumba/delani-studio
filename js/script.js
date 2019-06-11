cd66376c8d7a8c954efae966bcc03ea5c7cf30ca
markmumba <markmumba01@gmail.com>
2019-06-11 16:06:34 +0300

    new changes

--- js/script.js
+++ js/script.js
@@ -2,37 +2,25 @@ $(document).ready(function () {
     $("#design img").show();
     $("#design").click(function () {
         $("#design p").toggle();
-       
+
     });
-});
-$(document).ready(function () {
+
     $("#development").show();
     $("#development").click(function () {
         $("#development p").toggle();
 
     });
-});
-$(document).ready(function () {
+
     $("#product").show();
     $("#product").click(function () {
         $("#product p").toggle();
-     
-    });
-});
 
-$(document).ready(function() {
-    $('.textOne').hide();
-    $('.imageOne').animate({
-        opacity:1
-        
     });
 
-    $('.imageOne').hover(function() {
-        $(this).stop().animate({opacity:.4},200);
-        $('.textOne').fadeIn();
-
-    }, function() {
-        $(this).stop().animate({opacity:1},500)
-        $('.textOne').fadeOut();
+    $(".button").click(function () {
+        var alert = $(".alert");
+        for (i = 0; i < 1; i++) {
+            alert.slideToggle();
+        }
     });
 });
